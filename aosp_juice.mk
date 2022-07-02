c80b321a93887bba380599342baa93e2c9afe894
R.A.P <rifqikanekii140202@gmail.com>
2022-07-03 04:57:05 +0700

    juice; Project Arcana Bringup
    Signed-off-by: R.A.P <rifqikanekii140202@gmail.com>

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -6,17 +6,17 @@
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common PixelPlus-UI stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common arcana stuff.
+$(call inherit-product, vendor/aosp/common.mk)
+
+# Inherit some common arcana flags
+ARCANA_MAINTAINER := R.A.P
 TARGET_BOOT_ANIMATION_RES := 1080
-IS_PHONE := true
-CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
 TARGET_INCLUDE_STOCK_ARCORE := false
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_SUPPORTS_QUICK_TAP := false
-TARGET_FACE_UNLOCK_SUPPORTED := true
-PPUI_MAINTAINER := Lynx
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_juice
